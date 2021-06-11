2da4d10fcdb470f2a7cd03dfa2f248b5a24535e5
dimraidcamyrneh <77772384+dimraidcamyrneh@users.noreply.github.com>
2021-06-11 14:26:06 -0700

    Add files via upload

--- shared.js
+++ shared.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function myDrop() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
@@ -15,6 +15,71 @@ function myFunction() {
       }
     }
   }
+
+  function myCap() {
+    document.getElementById("myCaption").classList.toggle("caption-show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+ /*  window.onclick = function(event) {
+    if (!event.target.matches('.caption-link')) {
+      var captions = document.getElementsByClassName("caption-content");
+      var i;
+      for (i = 0; i < captions.length; i++) {
+        var openCaption = captions[i];
+        if (openCaption.classList.contains('caption-show')) {
+          openCaption.classList.remove('caption-show');
+        }
+      }
+    }
+  } */
+
+  function myCap1() {
+    document.getElementById("myCaption1").classList.toggle("caption-show");
+  }
+  function myCap2() {
+    document.getElementById("myCaption2").classList.toggle("caption-show");
+  }
+  function myCap3() {
+    document.getElementById("myCaption3").classList.toggle("caption-show");
+  }
+  function myCap4() {
+    document.getElementById("myCaption4").classList.toggle("caption-show");
+  }
+  function myCap5() {
+    document.getElementById("myCaption5").classList.toggle("caption-show");
+  }
+  function myCap6() {
+    document.getElementById("myCaption6").classList.toggle("caption-show");
+  }
+  function myCap7() {
+    document.getElementById("myCaption7").classList.toggle("caption-show");
+  }
+  function myCap8() {
+    document.getElementById("myCaption8").classList.toggle("caption-show");
+  }
+  function myCap9() {
+    document.getElementById("myCaption9").classList.toggle("caption-show");
+  }
+  function myCap10() {
+    document.getElementById("myCaption10").classList.toggle("caption-show");
+  }
+  function myCap11() {
+    document.getElementById("myCaption11").classList.toggle("caption-show");
+  }
+  function myCap12() {
+    document.getElementById("myCaption12").classList.toggle("caption-show");
+  }
+  function myCap13() {
+    document.getElementById("myCaption13").classList.toggle("caption-show");
+  }
+  function myCap14() {
+    document.getElementById("myCaption14").classList.toggle("caption-show");
+  }
+  function myCap15() {
+    document.getElementById("myCaption15").classList.toggle("caption-show");
+  }
+
 
 function openModal() {
     document.getElementById("myModal").style.display = "block";
